798e47263b3a111b42e490fffe71034a2d950c7b
Sudhanmurugan-Sparkout <sudhan@sparkouttech.com>
2024-04-18 19:37:06 +0530

    task2,task3 completed and task1 inprogress

--- TASK1/login.php
+++ TASK1/login.php
@@ -8,6 +8,11 @@ if(isset($_POST["submit"])){
 $mail=$_POST["mail"];
 $password=$_POST["password"];
 
+$sql="select*from signup_information where user_name=$mail and password=$password";
+$check=mysqli_query($conn,$sql);
+
+
+
 
 }
 
--- TASK1/signup.php
+++ TASK1/signup.php
@@ -1,6 +1,6 @@
 <?php 
  
- include_once 'signup.html'
+ include_once 'signup.html';
 
 $serverName="localhost";
 $username="root";
@@ -8,16 +8,16 @@ $password= "";
 
 $conn=mysqli_connect($serverName,$username,$password);
 
-$sql="CREATE DATABASE IF NOT EXISTS user_Details;";
+$sql="CREATE DATABASE IF NOT EXISTS user_Details";
 $database=mysqli_query($conn,$sql);
 
 mysqli_select_db($conn,'user_Details');
 
 $create="CREATE TABLE IF NOT EXISTS signup_information(id int auto_increment primary key,
-                                         user_name varchar(30) primary key,
+                                         user_name varchar(30),
                                          email varchar(40),
-                                         password varchar(10)
-                                         gender varchar(10));";
+                                         password varchar(10),
+                                         gender varchar(10))";
 
 $create_table=mysqli_query($conn,$create);                                         
 if($create_table){
@@ -30,9 +30,10 @@ $name=$_POST["name"];
 $email=$_POST["mail"];
 $password=$_POST["password"];
 $gender = $_POST["gender"];
-}
 $insert="INSERT INTO signup_information(first_name,email,password,gender) values('$name','$email','$password','$gender')";
 mysqli_query($conn,$insert);
+
+}
 
 
 
